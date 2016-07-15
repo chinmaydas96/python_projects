b3e6562e0d4169961b34df3b957a60d6f991c401
chinmaydas96 <chinmaydasbat@gmail.com>
2016-07-16 00:26:05 +0530

    sms api revised

--- sms/sms.py
+++ sms/sms.py
@@ -1,9 +1,9 @@
 """ simple python script for sending sms to specified mobile number given   """
 
 from twilio.rest import TwilioRestClient
-accountSID = 'ACa65f710b22bf1951e02fc8bfba5f2cdd'
-authToken = '8a1f73f2d53efa635ed91edbbc33edc1'
+accountSID = '**************************'
+authToken = '***************************'
 twilioCli = TwilioRestClient(accountSID, authToken)
-myTwilioNumber = '+16162582481'
-myCellPhone = '+919778428629'
-message = twilioCli.messages.create(body='Mr Pankaj this RRD sir,from CET call me immediately', from_=myTwilioNumber, to=myCellPhone)
+myTwilioNumber = '**********'
+myCellPhone = '*************'
+message = twilioCli.messages.create(body='msg', from_=myTwilioNumber, to=myCellPhone)
