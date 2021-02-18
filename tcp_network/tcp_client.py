40ddcc0ff1ca18cbcc98b645e6e1e7de60502d89
chinmay Das <chinmaydasbat@gmail.com>
2021-02-18 19:49:00 +0530

    update with python 3

--- tcp_network/tcp_client.py
+++ tcp_network/tcp_client.py
@@ -1,19 +1,19 @@
 import socket
 
 def main():
-	host = '192.168.43.232'  
+	host = '127.0.0.1'  
 	port = 8004
 
 	s = socket.socket()
 	s.connect((host,port))
 
-	message = raw_input("-> ")
+	message = input("-> ")
 	while message != 'q':
-		s.send(message)
+		s.send(bytes(message, 'utf-8'))
 		data = s.recv(1024)
-		print "Linux : " + str(data)
-		message = raw_input("-> ")
+		print ("Client : " + data.decode("utf-8"))
+		message = input("-> ")
 	s.close()
 
 if __name__ == '__main__':
-			main()		
+	main()		
